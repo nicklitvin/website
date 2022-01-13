70141ab73a1cc30fb7565fa8fe7c5b609c84e9ce
nicklitvin <nick.litvinov9@gmail.com>
2022-01-13 13:30:28 -0800

    added login

--- modules/AccountManager.js
+++ modules/AccountManager.js
@@ -2,6 +2,11 @@ const Account = require("./Account.js")
 const CryptoJS = require("crypto-js")
 const email = require("./emailService.js")
 
+const EMAIL_ENABLED = false
+const ACCEPTED_EMAIL_DOMAINS = [
+    "@gmail.com", "@yahoo.com", "@outlook.com"
+]
+
 class AccountManager{
     constructor(key,url){
         this.key = key
@@ -9,6 +14,8 @@ class AccountManager{
         this.accounts = []
         this.email = new email.EmailService(url)
     }
+
+    // VERIFICATION
 
     processVerificationRequest(req,res){
         const txt = req.body
@@ -23,23 +30,37 @@ class AccountManager{
 
     createUserAccount(txt,res){
         const email = CryptoJS.AES.decrypt(txt.email, this.key).toString(CryptoJS.enc.Utf8)
-        if(!this.isEmailTaken(email)){
+        if(!this.isEmailTaken(email) && this.isEmailLegit(email)){
             const acc = new Account.Account(email,txt.password)
             acc.setSid()
             this.accounts.push(acc)
-            this.sendUserSid(res,acc.sid)
+            this.sendUserSidAndRedirect(res,acc.sid)
+        }
+        else{
+            this.redirectUserToHome(res)
         }
     }
+
+    isEmailLegit(email){
+        for(let domain of ACCEPTED_EMAIL_DOMAINS){
+            if(email.includes(domain)){
+                return true
+            }
+        }
+    }
+
+    // MAIN PAGE REQUESTS
 
     processAccountRequest(req,res){
         const txt = req.body
         console.log(txt)
         try{
-            if(txt.action == "create"){
-                this.sendVerificationCode(txt,res)
+            const keys = Object.keys(txt)
+            if(keys.includes("password") || keys.includes("sid")){
+                this.tryLoginUser(txt,res)
             }
             else{
-                this.tryLoginUser(txt,res)
+                this.sendVerificationCode(txt,res)
             }
         }
         catch{
@@ -51,8 +72,13 @@ class AccountManager{
         if(this.isEmailTaken(txt.email)){
             return this.sendEmailTakenError(res)
         }
+        if(!this.isEmailLegit(txt.email)){
+            return this.sendBadInputError(res)
+        }
         const encoded = CryptoJS.AES.encrypt(txt.email, this.key).toString()
-        this.email.send(txt.email,encoded)
+        if(EMAIL_ENABLED){
+            this.email.send(txt.email,encoded)
+        }
         console.log(`${this.url}/confirm/?e=${encoded}`)
         this.sendVerificationMessage(res)
     }
@@ -64,6 +90,35 @@ class AccountManager{
             }
         }
         return false
+    }
+
+    // LOGIN
+
+    tryLoginUser(txt,res){
+        const sid = this.isLegitLogin(txt)
+        if(sid){
+            this.sendSidAndLogin(res,sid)
+        }
+        else{
+            this.sendBadInputError(res)
+        }
+    }
+
+    isLegitLogin(txt){
+        if(txt.sid){
+            for(let acc of this.accounts){
+                if(acc.sid == txt.sid){
+                    return acc.sid
+                }
+            }
+        }   
+        else{
+            for(let acc of this.accounts){
+                if(acc.email == txt.email && acc.password == txt.password){
+                    return acc.sid
+                }
+            }
+        }
     }
     
     // ERROR
@@ -83,8 +138,25 @@ class AccountManager{
         res.send(JSON.stringify(data))
     }
 
-    sendUserSid(res,sid=null){
-        const data = {sid: sid}
+    sendUserSidAndRedirect(res,sid=null){
+        const data = {
+            sid: sid,
+            message: "Account created",
+            url: this.url
+        }
+        res.send(JSON.stringify(data)) 
+    }
+
+    sendSidAndLogin(res,sid=null){
+        const data = {
+            sid: sid,
+            message: "Succesfully logged in"
+        }
+        res.send(JSON.stringify(data)) 
+    }
+
+    redirectUserToHome(res){
+        const data = {url: this.url}
         res.send(JSON.stringify(data)) 
     }
 }
